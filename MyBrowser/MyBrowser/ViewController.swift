a6c4550a89eb1f66f7fd8dac2da099adde06f8be
foreverjungleking <feifeizhang@Feifeis-Mac-mini.local>
2015-12-27 00:48:15 +0800

    create project

---
+++ MyBrowser/MyBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyBrowser
+//
+//  Created by Feifei Zhang on 27/12/15.
+//  Copyright © 2015 Feifei Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
